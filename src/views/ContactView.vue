4a72d8f3cda27c8ff5957c0a84f596ba65ed7eb6
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-03-25 04:12:33 -0300

    Contact Form update

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -4,7 +4,7 @@ import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } fro
 import { CheckIcon } from '@heroicons/vue/24/outline'
 // ExclamationTriangleIcon
 
-const open = ref(true)
+const open = ref(false)
 </script>
 
 <script>
@@ -12,8 +12,10 @@ export default {
     data() {
         return {
             classes: {
-                label: 'block text-sm font-semibold leading-6 text-gray-900',
-                input: 'block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none placeholder:text-gray-400 focus:ring-indigo-500 sm:text-sm sm:leading-6 transition duration-400',
+                label: 'block text-sm font-semibold leading-6 text-slate-700',
+                input: 'block w-full rounded-md border-0 py-2 px-3.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none placeholder:text-gray-400 focus:ring-indigo-500 sm:text-sm sm:leading-6 transition duration-400',
+                select: 'block w-full rounded-md border-0 py-2 pl-3 pr-10 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none placeholder:text-gray-400 focus:ring-indigo-500 sm:text-sm sm:leading-6 transition duration-400'
+
             }
         }
     }
@@ -25,60 +27,45 @@ export default {
 
         <div class="mx-auto max-w-2xl text-center">
             <h2 class="text-3xl font-bold tracking-tight text-gray-900">Fale com a Pepper</h2>
-            <p class="mt-2 text-lg leading-8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+            <p class="mt-2 text-lg leading-8 text-gray-600">Se nossos artigos não te ajudaram, entre em contato e vamos responder assim que possível.</p>
         </div>
 
         <form action="#" method="POST" class="mx-auto mt-12 max-w-2xl">
-            <div class="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
+            <div class="flex flex-col gap-y-5">
 
-                <div class="sm:col-span-2">
-                    <label for="name" :class="classes.label">Nome completo:</label>
-                    <div class="mt-2.5">
-                        <input type="text" name="name" id="name" autocomplete="name" :class="classes.input" />
-                    </div>
+                <div class="flex flex-col gap-2">
+                    <label :class="classes.label">Nome completo:</label>
+                    <input type="text" name="name" id="name" autocomplete="name" :class="classes.input" />
                 </div>
 
-                <div class="sm:col-span-2">
-                    <label for="name" :class="classes.label">Digite seu e-mail:</label>
-                    <div class="mt-2.5">
-                        <input type="email" name="email" id="email" autocomplete="email" :class="classes.input" />
-                    </div>
+                <div class="flex flex-col gap-2">
+                    <label :class="classes.label">Digite seu e-mail:</label>
+                    <input type="email" name="email" id="email" autocomplete="email" :class="classes.input" />
                 </div>
 
-                <div class="sm:col-span-2">
-                    <label for="name" :class="classes.label">CPF ou CNPJ:</label>
-                    <div class="mt-2.5">
-                        <input type="text" name="company" id="company" autocomplete="organization" :class="classes.input" />
-                    </div>
+                <div class="flex flex-col gap-2">
+                    <label :class="classes.label">Celular com WhatsApp:</label>
+                    <input type="text" name="company" id="company" autocomplete="organization" :class="classes.input" />
                 </div>
 
-                <div class="sm:col-span-2">
-                    <label for="name" :class="classes.label">Celular com WhatsApp:</label>
-                    <div class="relative mt-2.5">
-                        <div class="absolute inset-y-0 left-0 flex items-center">
-                            <select id="country" name="country" class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-                                <option>US</option>
-                                <option>CA</option>
-                                <option>EU</option>
-                            </select>
-                        </div>
-                        <input type="tel" name="phone-number" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-0 py-2 px-3.5 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                    </div>
+                <div class="flex flex-col gap-2">
+                    <label :class="classes.label">Motivo do contato:</label>
+                    <select :class="classes.select" id="contact-reason">
+                        <option value="1">Sou comprador e preciso de ajuda</option>
+                    </select>
                 </div>
 
-                <div class="sm:col-span-2">
-                    <label for="name" :class="classes.label">Mensagem:</label>
-                    <div class="mt-2.5">
-                        <textarea name="message" id="message" rows="4" :class="classes.input" />
-                    </div>
+                <div class="flex flex-col gap-2">
+                    <label :class="classes.label">Mensagem:</label>
+                    <textarea name="message" id="message" rows="4" :class="classes.input" />
                 </div>
 
             </div>
             <div class="mt-10">
-                <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar mensagem</button>
-            </div>
-    </form>
-</div>
+            <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar mensagem</button>
+        </div>
+        </form>
+    </div>
 
     <hr class="w-full mt-8" />
 
@@ -99,8 +86,8 @@ export default {
                                     <CheckIcon class="h-6 w-6 text-green-600" />
                                 </div>
                                 <!-- <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
-                                                <ExclamationTriangleIcon class="h-6 w-6 text-red-600" />
-                                            </div> -->
+                                                                                                                            <ExclamationTriangleIcon class="h-6 w-6 text-red-600" />
+                                                                                                                        </div> -->
                                 <div class="mt-3 text-center sm:mt-5">
                                     <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900 mb-2">
                                         Mensagem enviada com sucesso.
