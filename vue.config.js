6f0f35cf8d7597d04f6779ac669f306302a09f34
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-03-25 21:48:12 -0300

    .

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/pepper-help/',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = "My Vue App";
+        return args;
+      })
+  }
 })
