df4b2652ed4601a1e43d9345113ba27d1bcc0982
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-03-25 22:15:12 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -1,6 +1,49 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+//import router from './router'
 import '@/style.css'
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+
+import HomeView from './views/HomeView.vue'
+import DisplayContentView from './views/DisplayContentView.vue'
+import ListByCategoryView from './views/ListByCategoryView.vue'
+import ContactView from './views/ContactView.vue'
+import PageNotFoundView from './views/PageNotFoundView.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/categories/:category',
+        name: 'category',
+        component: ListByCategoryView
+    },
+    {
+        path: '/:article',
+        name: 'article',
+        component: DisplayContentView
+    },
+    {
+        path: '/contato',
+        name: 'contact',
+        component: ContactView
+    },
+    {
+        path: '/404',
+        name: '404',
+        component: PageNotFoundView
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
 
 createApp(App).use(router).mount('#app')
